a23e127bd22290051773e413b39c606dd6f9b59f
Graeme Morris <93727813+v-maxson@users.noreply.github.com>
2025-06-01 23:02:45 -0600

    fix: null-terminate errors

--- src/error.c
+++ src/error.c
@@ -8,7 +8,13 @@ void cpr_set_error(const char *fmt, ...)
 {
 	va_list args;
 	va_start(args, fmt);
+
+	// format the error message
 	snprintf(_error_msg, CPR_ERROR_MSG_SIZE, fmt, args);
+
+	// Ensure null termination
+	_error_msg[CPR_ERROR_MSG_SIZE - 1] = '\0';
+
 	va_end(args);
 }
 

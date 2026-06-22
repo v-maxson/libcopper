bc29148d0279a3a306044ba705293d1c6d783223
Graeme Morris <vmaxson.dev@gmail.com>
2026-06-22 14:34:05 -0600

    feat: simple time functions

--- include/copper/time.h
+++ include/copper/time.h
@@ -1,46 +1,81 @@
 #ifndef CPR_TIME_H
 #define CPR_TIME_H
 
-//* This module provides easy-to-use macros to convert between time units.
-//* Milliseconds is the primary unit; literal macros (cpr_ms, cpr_s, etc.) produce milliseconds.
+#include "platform.h"
+#include "result.h"
+#include <stdint.h>
 
 // --- Conversions to milliseconds ---
-#define cpr_ns_to_ms(x)  ((x) / 1000000ULL)
-#define cpr_us_to_ms(x)  ((x) / 1000ULL)
-#define cpr_s_to_ms(x)   ((x) * 1000ULL)
+#define cpr_ns_to_ms(x) ((x) / 1000000ULL)
+#define cpr_us_to_ms(x) ((x) / 1000ULL)
+#define cpr_s_to_ms(x) ((x) * 1000ULL)
 #define cpr_min_to_ms(x) ((x) * 60000ULL)
-#define cpr_hr_to_ms(x)  ((x) * 3600000ULL)
+#define cpr_hr_to_ms(x) ((x) * 3600000ULL)
 
 // --- Conversions from milliseconds ---
-#define cpr_ms_to_ns(x)  ((x) * 1000000ULL)
-#define cpr_ms_to_us(x)  ((x) * 1000ULL)
-#define cpr_ms_to_s(x)   ((x) / 1000ULL)
+#define cpr_ms_to_ns(x) ((x) * 1000000ULL)
+#define cpr_ms_to_us(x) ((x) * 1000ULL)
+#define cpr_ms_to_s(x) ((x) / 1000ULL)
 #define cpr_ms_to_min(x) ((x) / 60000ULL)
-#define cpr_ms_to_hr(x)  ((x) / 3600000ULL)
+#define cpr_ms_to_hr(x) ((x) / 3600000ULL)
 
 // --- Other conversions ---
-#define cpr_ns_to_us(x)  ((x) / 1000ULL)
-#define cpr_us_to_ns(x)  ((x) * 1000ULL)
-#define cpr_ns_to_s(x)   ((x) / 1000000000ULL)
-#define cpr_s_to_ns(x)   ((x) * 1000000000ULL)
+#define cpr_ns_to_us(x) ((x) / 1000ULL)
+#define cpr_us_to_ns(x) ((x) * 1000ULL)
+#define cpr_ns_to_s(x) ((x) / 1000000000ULL)
+#define cpr_s_to_ns(x) ((x) * 1000000000ULL)
 #define cpr_ns_to_min(x) ((x) / 60000000000ULL)
 #define cpr_min_to_ns(x) ((x) * 60000000000ULL)
-#define cpr_ns_to_hr(x)  ((x) / 3600000000000ULL)
-#define cpr_hr_to_ns(x)  ((x) * 3600000000000ULL)
-#define cpr_us_to_s(x)   ((x) / 1000000ULL)
-#define cpr_s_to_us(x)   ((x) * 1000000ULL)
-#define cpr_s_to_min(x)  ((x) / 60ULL)
-#define cpr_min_to_s(x)  ((x) * 60ULL)
-#define cpr_s_to_hr(x)   ((x) / 3600ULL)
-#define cpr_hr_to_s(x)   ((x) * 3600ULL)
+#define cpr_ns_to_hr(x) ((x) / 3600000000000ULL)
+#define cpr_hr_to_ns(x) ((x) * 3600000000000ULL)
+#define cpr_us_to_s(x) ((x) / 1000000ULL)
+#define cpr_s_to_us(x) ((x) * 1000000ULL)
+#define cpr_s_to_min(x) ((x) / 60ULL)
+#define cpr_min_to_s(x) ((x) * 60ULL)
+#define cpr_s_to_hr(x) ((x) / 3600ULL)
+#define cpr_hr_to_s(x) ((x) * 3600ULL)
 #define cpr_min_to_hr(x) ((x) / 60ULL)
 #define cpr_hr_to_min(x) ((x) * 60ULL)
 
 // --- Literal duration macros (produce values in milliseconds) ---
-#define cpr_ms(x)  (x)
-#define cpr_us(x)  cpr_us_to_ms(x)
-#define cpr_s(x)   cpr_s_to_ms(x)
+#define cpr_ms(x) (x)
+#define cpr_us(x) cpr_us_to_ms(x)
+#define cpr_s(x) cpr_s_to_ms(x)
 #define cpr_min(x) cpr_min_to_ms(x)
-#define cpr_hr(x)  cpr_hr_to_ms(x)
+#define cpr_hr(x) cpr_hr_to_ms(x)
+
+// --- Date Time ---
+
+typedef struct {
+	int16_t year; ///< -1 on failure.
+	uint8_t month; ///< 1-12
+	uint8_t day; ///< 1-31
+	uint8_t hour; ///< 0-23
+	uint8_t minute; ///< 0-59
+	uint8_t second; ///< 0-59
+	uint16_t ms; ///< 0-999
+	uint16_t yday; ///< 0-365; day of the year
+} CprDateTime;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// Returns UTC milliseconds since UNIX epoch.
+CPR_API uint64_t cpr_time_now(void);
+
+/// Returns milliseconds on a monotonic clock (epoch unspecified).
+/// Never goes backwards, used for measuring elapsed time.
+CPR_API uint64_t cpr_time_monotonic(void);
+
+/// If CprDateTime.year is -1, it indicates a failure.
+CPR_API CprDateTime cpr_time_local(uint64_t ms);
+
+/// If CprDateTime.year is -1, it indicates a failure.
+CPR_API CprDateTime cpr_time_utc(uint64_t ms);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // CPR_TIME_H
---
+++ src/time.c
@@ -0,0 +1,92 @@
+#include "copper/time.h"
+
+#include <bits/time.h>
+#include <time.h>
+
+#if defined(CPR_PLATFORM_WINDOWS)
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#if defined(CPR_PLATFORM_WINDOWS)
+#define CPR__FILETIME_UNIX_OFFSET 116444736000000000ULL
+#define CPR__FILETIME_PER_MS (cpr_ms_to_ns(1ULL) / 100ULL)
+#endif
+
+CPR_API uint64_t cpr_time_now(void)
+{
+#if defined(CPR_PLATFORM_WINDOWS)
+	FILETIME ft;
+	uint64_t t;
+	GetSystemTimeAsFileTime(&ft);
+	t = ((uint64_t)ft.dwHighDateTime << 32) | (uint64_t)ft.dwLowDateTime;
+	return (t - CPR__FILETIME_UNIX_OFFSET) / CPR__FILETIME_PER_MS;
+#else
+	struct timespec ts;
+	clock_gettime(CLOCK_REALTIME, &ts);
+	return cpr_s_to_ms(ts.tv_sec) + cpr_ns_to_ms(ts.tv_nsec);
+#endif
+}
+
+CPR_API uint64_t cpr_time_monotonic(void)
+{
+#if defined(CPR_PLATFORM_WINDOWS)
+	static LARGE_INTEGER s_freq = { { 0, 0 } };
+	LARGE_INTEGER counter;
+	if (s_freq.QuadPart == 0)
+		QueryPerformanceCounter(&counter);
+	return cpr_s_to_ms(counter.QuadPart) / s_freq.QuadPart;
+#else
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	return cpr_s_to_ms(ts.tv_sec) + cpr_ns_to_ms(ts.tv_nsec);
+#endif
+}
+
+/// Returned on conversion failure.
+static const CprDateTime CPR__DT_ERR = { -1, 0, 0, 0, 0, 0, 0, 0 };
+
+static CprDateTime cpr__fill(const struct tm *tm, uint64_t ms)
+{
+	return (CprDateTime){ .year = tm->tm_year + 1900,
+			      .month = tm->tm_mon + 1,
+			      .day = tm->tm_mday,
+			      .hour = tm->tm_hour,
+			      .minute = tm->tm_min,
+			      .second = tm->tm_sec,
+			      .ms = ms % cpr_s_to_ms(1),
+			      .yday = tm->tm_yday };
+}
+
+CPR_API CprDateTime cpr_time_local(uint64_t ms)
+{
+	time_t t = cpr_ms_to_s(ms);
+	struct tm tm_buf;
+
+#if defined(CPR_PLATFORM_WINDOWS)
+	if (localtime_s(&tm_buf, &t) != 0)
+		return CPR__DT_ERR;
+#else
+	if (!localtime_r(&t, &tm_buf))
+		return CPR__DT_ERR;
+#endif
+	return cpr__fill(&tm_buf, ms);
+}
+
+CPR_API CprDateTime cpr_time_utc(uint64_t ms)
+{
+	time_t t = cpr_ms_to_s(ms);
+	struct tm tm_buf;
+
+#if defined(CPR_PLATFORM_WINDOWS)
+	if (gmtime_s(&tm_buf, &t) != 0)
+		return CPR__DT_ERR;
+#else
+	if (!gmtime_r(&t, &tm_buf))
+		return CPR__DT_ERR;
+#endif
+
+	return cpr__fill(&tm_buf, ms);
+}
